cb095c24393d0c2371342607ba6420f333dba8b3
Marlen <marlenwieduwilt@aol.com>
2021-07-13 14:52:46 +0200

    task compleated

---
+++ EXERCISES/weather-conditions-13.07.21/script.js
@@ -0,0 +1,13 @@
+let temp = 1;
+
+if (temp>=20 && temp<30) {
+    console.log("it is warm");
+} else if(temp>=30) {
+    console.log("it is too hot");
+}
+else if( temp > 5) {
+  console.log("it is chilly!");
+}
+else { 
+    console.log("it is cold!");
+}
